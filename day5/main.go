3d3ffc75e42e01ebab7dd13b1923b7f3a7cfe293
Nick Klauer <nklauer@zendesk.com>
2022-12-05 20:46:07 -0600

    feat: day 5 part 2

--- day5/main.go
+++ day5/main.go
@@ -26,14 +26,31 @@ func main() {
 		}
 	}
 
+	var orderedStacks []*list.List
+	for _, stack := range stacks {
+		current := list.New()
+		for e := stack.Front(); e != nil; e = e.Next() {
+			current.PushBack(e.Value)
+		}
+		orderedStacks = append(orderedStacks, current)
+	}
 	fmt.Println("instructionStart", lines[instructionStart+1])
 	for _, line := range lines[instructionStart+1:] {
 		move(stacks, line)
+		moveInOrder(orderedStacks, line)
 	}
 
+	fmt.Printf("one-at-a-time: ")
 	for _, stack := range stacks {
 		fmt.Printf("%s", stack.Front().Value)
 	}
+	fmt.Println()
+
+	fmt.Printf("multiple-at-once: ")
+	for _, stack := range orderedStacks {
+		fmt.Printf("%s", stack.Front().Value)
+	}
+	fmt.Println()
 
 }
 
@@ -72,13 +89,17 @@ func loadStacks(lines []string) []*list.List {
 	return stacks
 }
 
-func move(stacks []*list.List, instruction string) {
-	// move 6 from 1 to 7
+func getInstruction(instruction string) (int, int, int) {
 	pieces := strings.Split(instruction, " ")
 	amt, from, to := pieces[1], pieces[3], pieces[5]
 	amtInt, _ := strconv.Atoi(amt)
 	fromInt, _ := strconv.Atoi(from)
 	toInt, _ := strconv.Atoi(to)
+	return amtInt, fromInt, toInt
+}
+
+func move(stacks []*list.List, instruction string) {
+	amtInt, fromInt, toInt := getInstruction(instruction)
 	for i := 1; i <= amtInt; i++ {
 		// move from one stack to another one
 		fromStack := stacks[fromInt-1]
@@ -87,7 +108,23 @@ func move(stacks []*list.List, instruction string) {
 	}
 }
 
+func moveInOrder(stacks []*list.List, instruction string) {
+	amtInt, fromInt, toInt := getInstruction(instruction)
+	fromStack := stacks[fromInt-1]
+	toStack := stacks[toInt-1]
+	moveStack(fromStack, toStack, amtInt)
+}
+
 func moveContainer(from, to *list.List) {
 	container := from.Remove(from.Front())
 	to.PushFront(container)
+}
+
+func moveStack(from, to *list.List, howMany int) {
+	newStack := list.New()
+	for i := 0; i < howMany; i++ {
+		container := from.Remove(from.Front())
+		newStack.PushBack(container)
+	}
+	to.PushFrontList(newStack)
 }
